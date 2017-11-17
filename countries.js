35ba223e503266ba0635e9f8b2741a9e1383fee9
Dianangelica <dianangelicag50283@students.laalliance.org>
2017-11-17 20:46:07 +0000

    Countries Array

---
+++ countries.js
@@ -0,0 +1,16 @@
+var fourletterCountries = [
+    "Chad", 
+    "Cuba",
+    "Greenland",
+    "Iraq",
+    "Mali",
+    "Oman",
+    "India"
+]
+fourletterCountries.push("Fiji")
+fourletterCountries.splice(4,0,"Iran")
+fourletterCountries.splice(7,1)
+fourletterCountries.reverse();
+
+console.log(fourletterCountries)
+
